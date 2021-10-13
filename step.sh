ce22cae89d8b701d8045eb855c2a0726c39de193
Nicolas Brocard <nbrocard@freebox.fr>
2021-10-13 16:15:22 +0200

    Initial commit

---
+++ step.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -x
+
+# install lokalise CLI
+curl -sfL https://raw.githubusercontent.com/lokalise/lokalise-cli-2-go/master/install.sh | sh
+
+# upload files
+for file in $source; do
+    echo "-- file to upload :${file}"
+    echo $(bin/lokalise2 -t $token --project-id $project_id file upload --lang-iso $lang_iso --file $file --tag-updated-keys Updated --distinguish-by-file --replace-modified --convert-placeholders false)
+    echo ""
+done
+
