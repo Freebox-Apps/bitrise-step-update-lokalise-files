a9ca88eaf3a15c27ed28ab8ca08a3bdc5ba93231
Nicolas Brocard <tchettane@gmail.com>
2022-01-11 11:06:17 +0100

    Update step.sh
    fix key taggging

--- step.sh
+++ step.sh
@@ -7,7 +7,7 @@ curl -sfL https://raw.githubusercontent.com/lokalise/lokalise-cli-2-go/master/in
 # upload files
 for file in $source; do
     echo "-- file to upload :${file}"
-    echo $(bin/lokalise2 -t $token --project-id $project_id file upload --lang-iso $lang_iso --file $file --tag-updated-keys Updated --distinguish-by-file --replace-modified --convert-placeholders false)
+    echo $(bin/lokalise2 -t $token --project-id $project_id file upload --lang-iso $lang_iso --file $file --tag-updated-keys=true --tags Updated --distinguish-by-file --replace-modified --convert-placeholders false)
     echo ""
 done
 
